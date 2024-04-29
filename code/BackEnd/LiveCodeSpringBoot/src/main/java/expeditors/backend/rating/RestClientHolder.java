6b6db6fa5977bc4c09f017235488e58edff49a66
whynot <anil@panix.com>
2024-04-29 06:40:30 -0400

    Added DB

--- code/BackEnd/LiveCodeSpringBoot/src/main/java/expeditors/backend/rating/RestClientHolder.java
+++ code/BackEnd/LiveCodeSpringBoot/src/main/java/expeditors/backend/rating/RestClientHolder.java
@@ -1,5 +1,6 @@
 package expeditors.backend.rating;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
@@ -10,12 +11,20 @@ public class RestClientHolder {
 
    private String ratingUrl;
 
-   public RestClientHolder() {
+   /**
+    * We use the RestTemplateBuilder here so that we can use the
+    * @RestClientTest annotation.  At the time of this writing,
+    * @RestClientTest does not support using the RestClient.builder()
+    * directly.
+    *
+    * @param restTemplateBuilder
+    */
+   public RestClientHolder(RestTemplateBuilder restTemplateBuilder) {
       var baseUrl = "http://localhost:10001";
       var rootUrl = "/courseRating";
       ratingUrl = rootUrl + "/{id}";
 
-      this.restClient = RestClient.builder()
+      this.restClient = RestClient.builder(restTemplateBuilder.build())
             .baseUrl(baseUrl)
             .defaultHeader("Accept", "application/json")
             .defaultHeader("Content-Type", "application/json")
